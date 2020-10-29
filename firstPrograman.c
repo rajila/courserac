00dddc4f7bb9e6614a2304b8bad5cddd6638c2f2
Ronald <rdajila@gmail.com>
2020-10-29 18:40:31 +0100

    First code

---
+++ firstPrograman.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+	printf("\"A veces me parece\n");
+	printf("que estamos en el centro\n");
+	printf("de la fiesta\n");
+	printf("sin embargo\n");
+	printf("en el centro de la fiesta\n");
+	printf("no hay nadie\n");
+	printf("En el centro de la fiesta\n");
+	printf("esta el vacio\n");
+	printf("Pero en el centro del vacio\n");
+	printf("hay otra fiesta.\"\n");
+	return 0;
+}
