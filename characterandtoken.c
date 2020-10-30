93ba7a75826293c22aa06a1be6d53a412264ee28
Ronald <rdajila@gmail.com>
2020-10-30 06:41:32 +0100

    excersises of week #2

---
+++ characterandtoken.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+  printf("Hi from Coursera course!!\n");
+  printf("%d\n", 5 < 7 + 2);
+  printf("%d\n", 10 < 7 + 2);
+  printf("%d\n", (5 < 7) + 2);
+  printf("%d\n", 8==+8);
+  printf("%d\n", -5%3);
+  printf("%d\n", 3%5);
+  return 0;
+}
